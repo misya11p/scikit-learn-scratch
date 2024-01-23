2cd65dad4886c979f09a6c942f486c2e2d6c0230
みっくん <130355798+mitsuki0215@users.noreply.github.com>
2024-01-23 22:51:55 +0900

    Simple linear regression #4 (#5)
    * 単回帰の実装をした
    
    * 型ヒントを追記
    
    * docstringを追記
    
    * Baseクラスを継承
    
    ---------
    
    Co-authored-by: Takahashi-Kaisei <s2322037@stu.musashino-u.ac.jp>
    Co-authored-by: mitsuki0215 <mitsuki0215>

---
+++ ssklearn/linear_model/_classes.py
@@ -0,0 +1,31 @@
+import numpy as np
+from .._base import BaseRegressor
+
+
+class SimpleLinearRegression(BaseRegressor):
+    def fit(self, x: np.ndarray, y: np.ndarray):
+        """
+        Training model.
+
+        Args:
+            x (np.ndarray): Training data.
+            y (np.ndarray): Target values.
+        """
+        var_x, cov = np.cov(x, y)[0]
+        bar_x = x.mean()
+        bar_y = y.mean()
+        self.a = cov / var_x
+        self.b = bar_y - self.a * bar_x
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        """
+        Predict using the model.
+
+        Args:
+            x (np.ndarray): Samples.
+
+        Returns:
+            np.ndarray: Predicted values.
+        """
+        y = self.a * x + self.b
+        return y
