c18f3188ea3dd04be1d4abe190085f339f0d2747
Takahashi-Kaisei <130371620+Takahashi-Kaisei@users.noreply.github.com>
2024-04-26 19:34:25 +0900

    線形回帰モデルの実装 (#10)

--- ssklearn/linear_model/_classes.py
+++ ssklearn/linear_model/_classes.py
@@ -29,3 +29,32 @@ class SimpleLinearRegression(BaseRegressor):
         """
         y = self.a * x + self.b
         return y
+
+
+class LinearRegression(BaseRegressor):
+    def __init__(self):
+        self.w = None
+
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        """
+        Training model.
+
+        Args:
+            X (np.ndarray): Training data.
+            y (np.ndarray): Target values.
+        """
+        X = np.insert(X, 0, 1, axis=1)
+        self.w = np.linalg.inv(X.T @ X) @ X.T @ y
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict using the model.
+
+        Args:
+            X (np.ndarray): Samples.
+
+        Returns:
+            np.ndarray: Predicted values.
+        """
+        X = np.insert(X, 0, 1, axis=1)
+        return X @ self.w
