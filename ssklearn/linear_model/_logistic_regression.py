01289927359e306ea3d5a731d9ca29abcdfd09aa
Takahashi-Kaisei <s2322037@stu.musashino-u.ac.jp>
2025-03-30 20:24:45 +0900

    ロジスティック回帰モデルのクラス名を修正し、fit_interceptオプションを追加。predict_probaメソッドを実装。

--- ssklearn/linear_model/_logistic_regression.py
+++ ssklearn/linear_model/_logistic_regression.py
@@ -1,28 +1,34 @@
 import numpy as np
-from .._base import BaseRegressor
+from .._base import BaseClassifire
 
 
-class LogisticRegression(BaseRegressor):
-    def __init__(self, tol: float = 0.0001, max_iter: int = 100):
+class LogisticRegression(BaseClassifire):
+    def __init__(
+        self,
+        tol: float = 0.0001,
+        max_iter: int = 100,
+        fit_intercept: bool = True
+    ):
         """logistic regression model."""
         self.tol = tol
         self.max_iter = max_iter
+        self.fit_intercept = fit_intercept
         self.w = None
         self.iter = None
 
 
     @staticmethod
-    def _sigmoid(xw: np.ndarray) -> np.ndarray:
+    def _sigmoid(x: np.ndarray) -> np.ndarray:
         """
         sigmoid function.
 
         Args:
-            xw (np.ndarray): dot product of X and w.
+            x (np.ndarray): Input data.
 
         Returns:
             np.ndarray: sigmoid function result.
         """
-        return 1 / (1 + np.exp(-xw))
+        return 1 / (1 + np.exp(-x))
 
     def fit(self, X: np.ndarray, y: np.ndarray):
         """
@@ -32,20 +38,45 @@ class LogisticRegression(BaseRegressor):
             X (np.ndarray): Training data.
             y (np.ndarray): Target values.
         """
+        if self.fit_intercept:
+            X = np.insert(X, 0, 1, axis=1)
+        else:
+            pass
         self.w = np.random.randn(X.shape[1])
         tol_vec = np.full(X.shape[1], self.tol)
         diff = np.full(X.shape[1], np.inf)
         self.iter = 0
         while np.any(diff > tol_vec) and (self.iter < self.max_iter):
-            y_hat = self._sigmoid(X @ self.w)
-            r = y_hat * (1 - y_hat)
-            w_new = self.w - np.linalg.solve(
-                (X.T * r) @ X,
-                X.T @ (y_hat - y)
+            pred = self.predict_proba(X)[:, 1]
+            r = pred * (1 - pred)
+            xr = X.T * r
+            w_new = np.dot(
+                (
+                    np.linalg.solve(np.dot(xr, X), xr)),
+                (np.dot(X, self.w) - (1 / r * (pred - y))
+                )
             )
             diff = w_new - self.w
             self.iter += 1
             self.w = w_new
+
+    def predict_proba(self, x: np.ndarray) -> np.ndarray:
+        """
+        Predict probability using the model.
+
+        Args:
+            x (np.ndarray): Samples.
+
+        Returns:
+            np.ndarray: Probability prediction results.
+        """
+        return np.stack(
+            [
+                1 - self._sigmoid(np.dot(x, self.w)),
+                self._sigmoid(np.dot(x, self.w))
+            ]
+            ,1
+        )
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -57,11 +88,10 @@ class LogisticRegression(BaseRegressor):
         Returns:
             np.ndarray: Binary prediction results.
         """
-        y_hat_xw = np.dot(X, self.w)
-        y_pred = self._sigmoid(y_hat_xw)
-        for _ in range(X.shape[0]):
-            if y_pred[_] > 0.5:
-                y_pred[_] = 1
-            elif y_pred[_] < 0.5:
-                y_pred[_] = 0
-        return y_pred
+        if self.fit_intercept:
+            X = np.insert(X, 0, 1, axis=1)
+        else:
+            pass
+        pred = self.predict_proba(X)[:, 1]
+        return (pred > 0.5).astype(int)
+
