d498a6638a6ba084436bf364f248be12219c7b78
Takahashi-Kaisei <130371620+Takahashi-Kaisei@users.noreply.github.com>
2025-04-08 19:12:45 +0900

    シグモイド関数の実装を改善し、フィッティングプロセスでの数値安定性を向上させるために重みの更新の過程で微小値を足すようにした

--- ssklearn/linear_model/_logistic_regression.py
+++ ssklearn/linear_model/_logistic_regression.py
@@ -28,7 +28,7 @@ class LogisticRegressionBinary(BaseClassifire):
         Returns:
             np.ndarray: sigmoid function result.
         """
-        return 1 / (1 + np.exp(-x))
+        return np.exp(np.minimum(x, 0)) / (1 + np.exp(- np.abs(x)))
 
     def fit(self, X: np.ndarray, y: np.ndarray):
         """
@@ -46,16 +46,12 @@ class LogisticRegressionBinary(BaseClassifire):
         self.iter = 0
         while np.any(diff > tol_vec) and (self.iter < self.max_iter):
             pred = self._sigmoid(np.dot(X, self.w))
-            r = pred * (1 - pred)
-            try:
-                w_new = self.w - np.linalg.solve(
-                    (X.T * r) @ X,
-                    X.T @ (pred - y)
-                )
-            except np.linalg.LinAlgError:
-                w_new, _, _, _ = np.linalg.lstsq(
-                    (X.T * r) @ X, X.T @ (pred - y)
-                )
+            r = pred * (1 - pred) + 1e-10
+            xr = X.T * r
+            w_new = np.dot(
+                (np.linalg.solve(np.dot(xr,X), xr)),
+                (np.dot(X, self.w) - (1 / r * (pred - y)))
+            )
             diff = w_new - self.w
             self.iter += 1
             self.w = w_new
