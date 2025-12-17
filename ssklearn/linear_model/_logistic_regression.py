88ef73f142a2cb4f9a8359f339c517f9330f2729
Kaise <130371620+Takahashi-Kaisei@users.noreply.github.com>
2025-12-17 17:10:51 +0900

    ロジスティック回帰モデルを実装 (#20)
    * ロジスティック回帰モデルを実装
    
    * ロジスティック回帰モデルのクラス名を修正し、fit_interceptオプションを追加。predict_probaメソッドを実装。
    
    * 不要なelse文を削除し、コードを簡素化
    
    * ロジスティック回帰モデルのクラス名を変更し、predict_probaメソッドを修正してfit_interceptオプションを考慮
    
    * __init__.pyにクラスを追加
    
    * predict内でpredict_probaを使うようにした
    
    * predict_probaとpredictの変数名を揃えた
    
    * 更新アルゴリズムを改善．
    
    * シグモイド関数の実装を改善し、フィッティングプロセスでの数値安定性を向上させるために重みの更新の過程で微小値を足すようにした
    
    * パラメータを1で初期化
    
    * 更新式を変形前のものに
    
    * シグモイドを元のものに
    
    ---------
    
    Co-authored-by: komiya <90894519+misya11p@users.noreply.github.com>

--- ssklearn/linear_model/_logistic_regression.py
+++ ssklearn/linear_model/_logistic_regression.py
@@ -1,2 +1,81 @@
-class LogisticRegression:
-    pass
+import numpy as np
+from .._base import BaseClassifire
+
+
+class LogisticRegressionBinary(BaseClassifire):
+    def __init__(
+        self,
+        tol: float = 0.0001,
+        max_iter: int = 100,
+        fit_intercept: bool = True
+    ):
+        """logistic regression model."""
+        self.tol = tol
+        self.max_iter = max_iter
+        self.fit_intercept = fit_intercept
+        self.w = None
+        self.iter = None
+
+    @staticmethod
+    def _sigmoid(x: np.ndarray) -> np.ndarray:
+        """
+        sigmoid function.
+
+        Args:
+            x (np.ndarray): Input data.
+
+        Returns:
+            np.ndarray: sigmoid function result.
+        """
+        return 1 / (1 + np.exp(-x))
+
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        """
+        Training model using Newton method.
+
+        Args:
+            X (np.ndarray): Training data.
+            y (np.ndarray): Target values.
+        """
+        if self.fit_intercept:
+            X = np.insert(X, 0, 1, axis=1)
+        self.w = np.ones(X.shape[1])
+        tol_vec = np.full(X.shape[1], self.tol)
+        diff = np.full(X.shape[1], np.inf)
+        self.iter = 0
+        while np.any(diff > tol_vec) and (self.iter < self.max_iter):
+            pred = self._sigmoid(np.dot(X, self.w))
+            grad = np.dot(X.T, (pred - y))
+            hess = np.dot(X.T * pred * (1 - pred), X)
+            hess_inv = np.linalg.pinv(hess)
+            w_new = self.w - np.dot(hess_inv, grad)
+            diff = w_new - self.w
+            self.iter += 1
+            self.w = w_new
+
+    def predict_proba(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict probability using the model.
+
+        Args:
+            X (np.ndarray): Samples.
+
+        Returns:
+            np.ndarray: Probability prediction results.
+        """
+        if self.fit_intercept:
+            X = np.insert(X, 0, 1, axis=1)
+        return self._sigmoid(np.dot(X, self.w))
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict using the model.
+
+        Args:
+            X (np.ndarray): Samples.
+
+        Returns:
+            np.ndarray: Binary prediction results.
+        """
+        return (self.predict_proba(X) > 0.5).astype(int)
+
